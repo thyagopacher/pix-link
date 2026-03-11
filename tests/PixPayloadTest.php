a95f662289fc2e8f6522fcd540c6fd75fe87d0ed
thyagopacher <thyago.pacher@gmail.com>
2026-03-11 14:00:53 -0300

    ajuste retorno function testGerarPayload

--- tests/PixPayloadTest.php
+++ tests/PixPayloadTest.php
@@ -13,9 +13,8 @@ class PixPayloadTest extends TestCase
         $nomeRecebedor = 'THYAGO HENRIQUE PACHER';
         $cidade = 'PONTA GROSSA';
         $valor = 1.65;      
-        $width = 360;
-        $height = 360;
-        $payloadTxt = PixPayload::gerar($chavePix, $nomeRecebedor, $cidade, $valor, $width, $height);
+
+        $payloadTxt = PixPayload::gerar($chavePix, $nomeRecebedor, $cidade, $valor);
         $this->assertIsString($payloadTxt, 'Payload deve ser uma string');
         $this->assertNotEmpty($payloadTxt, 'Payload não deve ser vazio');
         $this->assertStringContainsString('000201', $payloadTxt, 'Payload deve conter o identificador do Pix');
